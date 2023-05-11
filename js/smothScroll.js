6b965b5bfcacaa524bce3b1539860bc661dfa644
igor <igor.kolivushka47@gmail.com>
2023-05-11 16:38:54 +0300

    first

---
+++ js/smothScroll.js
@@ -0,0 +1,14 @@
+const linksHead = document.querySelectorAll(".menu-list__link");
+const mainScroll = document.querySelector(".main__scroll");
+const newArray = [...linksHead, mainScroll];
+
+newArray.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    const id = element.getAttribute("href").substring(1);
+    document.getElementById(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
